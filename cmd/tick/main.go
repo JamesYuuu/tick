6e4561e67af808886e1808c49e48a0e1d5d4f8d9
JamesYuuu <yujunda20@mails.ucas.ac.cn>
2026-03-05 10:48:19 +0800

    chore: rename tuitodo to tick

--- cmd/tick/main.go
+++ cmd/tick/main.go
@@ -17,45 +17,45 @@ import (
 
 func main() {
 	if !isatty.IsTerminal(os.Stdin.Fd()) || !isatty.IsTerminal(os.Stdout.Fd()) {
-		fmt.Fprintln(os.Stderr, "tuitodo: must be run in a TTY")
+		fmt.Fprintln(os.Stderr, "tick: must be run in a TTY")
 		os.Exit(2)
 	}
 
 	home, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "tuitodo: resolve home dir:", err)
+		fmt.Fprintln(os.Stderr, "tick: resolve home dir:", err)
 		os.Exit(1)
 	}
-	dataDir := filepath.Join(home, ".tuitodo")
+	dataDir := filepath.Join(home, ".tick")
 	if err := os.MkdirAll(dataDir, 0o700); err != nil {
-		fmt.Fprintln(os.Stderr, "tuitodo: create data dir:", err)
+		fmt.Fprintln(os.Stderr, "tick: create data dir:", err)
 		os.Exit(1)
 	}
 	dbPath := filepath.Join(dataDir, "todo.db")
 
 	f, err := os.OpenFile(dbPath, os.O_RDWR|os.O_CREATE, 0o600)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "tuitodo: create db file:", err)
+		fmt.Fprintln(os.Stderr, "tick: create db file:", err)
 		os.Exit(1)
 	}
 	_ = f.Close()
 
 	s, err := sqlite.Open(dbPath)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "tuitodo: open sqlite:", err)
+		fmt.Fprintln(os.Stderr, "tick: open sqlite:", err)
 		os.Exit(1)
 	}
 	defer func() { _ = s.Close() }()
 
 	a, err := app.New(app.Config{Store: s, Clock: timeutil.SystemClock{}, Location: time.Local})
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "tuitodo: init app:", err)
+		fmt.Fprintln(os.Stderr, "tick: init app:", err)
 		os.Exit(1)
 	}
 
 	p := tea.NewProgram(ui.New(a), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
-		fmt.Fprintln(os.Stderr, "tuitodo: run:", err)
+		fmt.Fprintln(os.Stderr, "tick: run:", err)
 		os.Exit(1)
 	}
 }

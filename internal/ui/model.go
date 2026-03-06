20d2ef399a7cd0d12df3038a7118c9ca04925f7d
JamesYuuu <yujunda20@mails.ucas.ac.cn>
2026-03-06 14:40:12 +0800

    refactor(ui): unify history refresh command flow

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -429,27 +429,7 @@ func (m Model) historySelectedDay() domain.Day {
 	return addDays(m.historyFrom, m.historyIndex)
 }
 
-func (m Model) cmdRefreshHistorySelectedDay() tea.Cmd {
-	day := m.historySelectedDay()
-	return func() tea.Msg {
-		ctx := context.Background()
-		done, err := m.app.HistoryDoneByDay(ctx, day)
-		if err != nil {
-			return historyRefreshMsg{err: err}
-		}
-		ab, err := m.app.HistoryAbandonedByDay(ctx, day)
-		if err != nil {
-			return historyRefreshMsg{err: err}
-		}
-		activeCreated, err := m.app.HistoryActiveByCreatedDay(ctx, day)
-		if err != nil {
-			return historyRefreshMsg{err: err}
-		}
-		return historyRefreshMsg{done: done, abandoned: ab, activeCreated: activeCreated}
-	}
-}
-
-func (m Model) cmdRefreshHistoryWithStats() tea.Cmd {
+func (m Model) cmdRefreshHistory(withStats bool) tea.Cmd {
 	day := m.historySelectedDay()
 	from, to := m.historyFrom, m.historyTo
 	return func() tea.Msg {
@@ -466,12 +446,23 @@ func (m Model) cmdRefreshHistoryWithStats() tea.Cmd {
 		if err != nil {
 			return historyRefreshMsg{err: err}
 		}
+		if !withStats {
+			return historyRefreshMsg{done: done, abandoned: ab, activeCreated: activeCreated}
+		}
 		stats, err := m.app.Stats(ctx, from, to)
 		if err != nil {
 			return historyRefreshMsg{err: err}
 		}
 		return historyRefreshMsg{done: done, abandoned: ab, activeCreated: activeCreated, stats: stats, hasStats: true}
 	}
+}
+
+func (m Model) cmdRefreshHistorySelectedDay() tea.Cmd {
+	return m.cmdRefreshHistory(false)
+}
+
+func (m Model) cmdRefreshHistoryWithStats() tea.Cmd {
+	return m.cmdRefreshHistory(true)
 }
 
 func bodyHeight(windowHeight int) int {
